f9f1f92e2a9d6855e7366d97281e95fc7634d2c0
Nader Hendawi <naderhen@gmail.com>
2012-02-16 20:42:49 -0500

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bigblue::Application.config.secret_token = '2afe6e30c0b0b9b24834aab295b20504925a395297409334452c93985bca9bdb8f9d5ab6d5bd95af1bf1ed61c2c895d0647ad541998511175d95935b50c3feb6'
